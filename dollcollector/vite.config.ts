e5560bc75d5fe5a5dc4a4958a33abf948fbc2b9e
Kyla Kim <kimky9805@gmail.com>
2025-09-17 16:25:40 +0900

    vite 설정 수정

--- dollcollector/vite.config.ts
+++ dollcollector/vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dollcollector/",
+  base: "./",
+  build: {
+    outDir: "../../", // 레포지토리 루트로 빌드
+  },
 });
